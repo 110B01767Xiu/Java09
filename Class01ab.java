af160819897eefe26493a94946cdcf639dea3281
110B01767Xiu <115057022+110B01767Xiu@users.noreply.github.com>
2022-10-31 03:02:02 +0800

    Create Class01ab.java

---
+++ Class01ab.java
@@ -0,0 +1,26 @@
+class CRectangle {
+   int width;
+   int height;
+
+   public CRectangle(int w,int h) {
+      width = w;
+      height = h;
+   }
+   public CRectangle() {
+      width = 10;
+      height = 8;
+   }
+   public void show() {
+      System.out.println("width = " + width);
+      System.out.println("height = " + height);
+   }
+}
+
+public class Main {
+   public static void main(String args[]) {
+      CRectangle a = new CRectangle();
+      a.show();
+      CRectangle b = new CRectangle();
+      b.show();
+   }
+}
